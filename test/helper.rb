00c15d58479ab916b2d50564fdcb950e3b92e23f
Shane Becker <veganstraightedge@gmail.com>
2011-02-15 17:31:56 -0800

    moved to minitest

---
+++ test/helper.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'new_base_60'
+
+module NewBaseSixty
+  class TestCase < MiniTest::Spec
+  end
+end
--- test/test_new_base_60.rb
+++ test/test_new_base_60.rb
@@ -1,29 +1,29 @@
-require "test/unit"
-require "new_base_60"
+require 'minitest/autorun'
+require 'new_base_60'
 
-class TestNewBase60 < Test::Unit::TestCase
+class TestCase < MiniTest::Spec
   def test_base60_to_base10
-    assert_equal     NewBase60.new("464").to_i, 14764
-    assert_not_equal NewBase60.new("464").to_i, 12345
+    assert NewBase60.new("464").to_i == 14764
+    refute NewBase60.new("464").to_i == 12345
   end
 
   def test_base60_to_date
-    assert_equal     NewBase60.new("464").to_date, Date.parse("2010/06/04")
-    assert_not_equal NewBase60.new("464").to_date, Date.parse("2010/06/05")
+    assert NewBase60.new("464").to_date == Date.parse("2010/06/04")
+    refute NewBase60.new("464").to_date == Date.parse("2010/06/05")
   end
 
   def test_date_to_base60
-    assert_equal     Date.parse("2010/06/04").to_sxg, NewBase60.new("464").to_s
-    assert_not_equal Date.parse("2010/06/05").to_sxg, NewBase60.new("464")
+    assert Date.parse("2010/06/04").to_sxg == NewBase60.new("464").to_s
+    refute Date.parse("2010/06/05").to_sxg == NewBase60.new("464")
   end
 
   def test_base10_to_base60
-    assert_equal     14764.to_sxg, "464"
-    assert_not_equal 12345.to_sxg, "464"
+    assert 14764.to_sxg == "464"
+    refute 12345.to_sxg == "464"
   end
 
   def test_base10_to_base60_with_leading_zeroes
-    assert_equal     14764.to_sxgf(9), "000000464"
-    assert_not_equal 12345.to_sxgf(9), "000000464"
+    assert 14764.to_sxgf(9) == "000000464"
+    refute 12345.to_sxgf(9) == "000000464"
   end
 end
